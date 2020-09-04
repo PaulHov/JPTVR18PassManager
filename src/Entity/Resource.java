f0c5c91cd0897918d082fd3c1c08e6bf994f9ed9
pupil <pupil@ivkhk.ee>
2020-09-04 15:25:50 +0300

    Заготовка для проекта

---
+++ src/Entity/Resource.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author pupil
+ */
+public class Resource {
+    private String name;
+    private String url;
+    private String login;
+    private String password;
+    
+    public Resource(){
+        
+        
+        
+    }
+
+    public Resource(String name, String url, String login, String password) {
+        this.name = name;
+        this.url = url;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    // End
+}
--- src/jptvr18passmanager/App.java
+++ src/jptvr18passmanager/App.java
@@ -4,11 +4,52 @@
  * and open the template in the editor.
  */
 package jptvr18passmanager;
+import Entity.Resource;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
  * @author pupil
  */
 class App {
-    
+    private List<Resource> listResources = new ArrayList<>();
+    public void run(){
+            System.out.println("---Mенедрер паролей---");
+            String repeat = "r";
+            do{
+            System.out.println("---Выберите задачу:---");
+            System.out.println("0 - Выйти из программы");
+            System.out.println("1 - Добавить ресурс");
+            System.out.println("2 - Список ресурсов");
+            Scanner scanner = new Scanner(System.in);
+            int task = scanner.nextInt();
+            scanner.nextLine();
+            switch (task) {                //sw + Tab
+                case 0:                    //ctrl + shift + strelkavniz
+                    repeat = "q";
+                    break;
+                case 1:
+                    System.out.println("Выбрана задача 1");
+                    Resource resource = new Resource();
+                    resource.setName("ivkhk");
+                    resource.setUrl("https://lutsehariduskeskus.ee");
+                    resource.setLogin("admin");
+                    resource.setPassword("123123");
+                    listResources.add(resource);
+                    break;
+                case 2:
+                    System.out.println("Выбрана задача 2");
+                    System.out.println("");
+                    for(Resource r : listResources){
+                        System.out.println(r.toString());
+                    }
+                    
+                    break;
+            }
+        }while(!"q".equals(repeat));
+    }
 }
